1874c75971c31070c5ed8062453cbe273717017d
Lansdon Page <lpage@qualtacon.com>
2021-03-02 17:10:34 -0800

    Add tests and error handling.

--- R/ExportToVorteks.R
+++ R/ExportToVorteks.R
@@ -5,8 +5,15 @@
 #' @importFrom utils write.table
 ExportDataframeToVorteks <- function(df, filename = "dataframe_export") {
 
-  check <- tryCatch({
+  # Validate dataframe arg
+  if(missing(df) || is.null(df) || !is.data.frame(df))
+    stop("Invalid dataframe")
 
+  # Validate filename arg
+  if(is.null(filename) || !(is.character(filename) && length(filename) == 1) || nchar(filename) < 1)
+    stop("Invalid filename")
+
+  check <- tryCatch({
     #create filename with vorteks dataframe extension
     path <- paste(filename,".vvedf", sep = "")
 
@@ -16,18 +23,19 @@ ExportDataframeToVorteks <- function(df, filename = "dataframe_export") {
     # open saved file - Should open Vorteks if appropriate version is installed via file association
     shell.exec(path)
 
+    print(paste("Vorteks Export:", path))
   },
   warning = function(war)
   {
-    print(paste("Vorteks Export WARNING: ", war))
+    print(paste("Vorteks Export WARNING:", war))
   },
   error = function(err)
   {
-    print(paste("Vorteks Export ERROR: ", err))
+    print(paste("Vorteks Export ERROR:", err))
   },
   finally = function(f)
   {
-    print(paste("Vorteks Export: ", path))
+    #    print(paste("Vorteks Export: ", path))
   })
 
 }
