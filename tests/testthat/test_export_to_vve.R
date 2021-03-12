1fe996336a36ed30217710259bc952de6ba4c2dc
Lansdon Page <lpage@qualtacon.com>
2021-03-12 09:30:18 -0800

    0.1.5 Update all tests to check Windows OS

--- tests/testthat/test_export_to_vve.R
+++ tests/testthat/test_export_to_vve.R
@@ -19,17 +19,25 @@ test_that("path is correct", {
 #> Test passed 😸
 
 test_that("missing dataframe has error", {
-  expect_error(ExportDataframeToVVE(), 'Invalid dataframe')
-  expect_error(ExportDataframeToVVE(NULL), 'Invalid dataframe')
-  expect_error(ExportDataframeToVVE("not a dataframe"), 'Invalid dataframe')
+  if(IsWindows()) {
+    expect_error(ExportDataframeToVVE(), 'Invalid dataframe')
+    expect_error(ExportDataframeToVVE(NULL), 'Invalid dataframe')
+    expect_error(ExportDataframeToVVE("not a dataframe"), 'Invalid dataframe')
+  } else {
+    expect_error(ExportDataframeToVVE(df), 'Unsupported OS (Windows required)')
+  }
 })
 #> Test passed 😸
 
 test_that("missing filename has error", {
   df = data.frame(a=c(1,2,3,4,5), b=c(11,22,33,44,55))
-  expect_error(ExportDataframeToVVE(df, NULL), 'Invalid filename')
-  expect_error(ExportDataframeToVVE(df, ""), 'Invalid filename')
-  expect_error(ExportDataframeToVVE(df, df), 'Invalid filename')
+  if(IsWindows()) {
+    expect_error(ExportDataframeToVVE(df, NULL), 'Invalid filename')
+    expect_error(ExportDataframeToVVE(df, ""), 'Invalid filename')
+    expect_error(ExportDataframeToVVE(df, df), 'Invalid filename')
+  } else {
+    expect_error(ExportDataframeToVVE(df), 'Unsupported OS (Windows required)')
+  }
 })
 #> Test passed 😸
 
