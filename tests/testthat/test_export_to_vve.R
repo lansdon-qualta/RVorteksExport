92e38cbe247a18a55c4983fc98929dcf17d9dd60
Lansdon Page <lpage@qualtacon.com>
2021-03-15 14:40:51 -0700

    Remove explicit windows check from tests.

--- tests/testthat/test_export_to_vve.R
+++ tests/testthat/test_export_to_vve.R
@@ -7,37 +7,24 @@ cleanup <- function(filename) {
 
 test_that("path is correct", {
   df = data.frame(a=c(1,2,3,4,5), b=c(11,22,33,44,55))
-  if(IsWindows()) {
-    expect_output(ExportDataframeToVVE(df, 'mydf'), 'Vorteks Export: mydf.vvedf')
-    expect_output(ExportDataframeToVVE(df), 'Vorteks Export: dataframe_export.vvedf')
-    cleanup("mydf.vvedf")
-    cleanup("dataframe_export.vvedf")
-  } else {
-    expect_error(ExportDataframeToVVE(df), 'Unsupported OS (Windows required)')
-  }
+  expect_output(ExportDataframeToVVE(df, 'mydf'), 'Vorteks Export: mydf.vvedf')
+  expect_output(ExportDataframeToVVE(df), 'Vorteks Export: dataframe_export.vvedf')
+  cleanup("mydf.vvedf")
+  cleanup("dataframe_export.vvedf")
 })
 #> Test passed 😸
 
 test_that("missing dataframe has error", {
-  if(IsWindows()) {
-    expect_error(ExportDataframeToVVE(), 'Invalid dataframe')
-    expect_error(ExportDataframeToVVE(NULL), 'Invalid dataframe')
-    expect_error(ExportDataframeToVVE("not a dataframe"), 'Invalid dataframe')
-  } else {
-    expect_error(ExportDataframeToVVE(df), 'Unsupported OS (Windows required)')
-  }
+  expect_error(ExportDataframeToVVE(), 'Invalid dataframe')
+  expect_error(ExportDataframeToVVE(NULL), 'Invalid dataframe')
+  expect_error(ExportDataframeToVVE("not a dataframe"), 'Invalid dataframe')
 })
 #> Test passed 😸
 
 test_that("missing filename has error", {
-  df = data.frame(a=c(1,2,3,4,5), b=c(11,22,33,44,55))
-  if(IsWindows()) {
-    expect_error(ExportDataframeToVVE(df, NULL), 'Invalid filename')
-    expect_error(ExportDataframeToVVE(df, ""), 'Invalid filename')
-    expect_error(ExportDataframeToVVE(df, df), 'Invalid filename')
-  } else {
-    expect_error(ExportDataframeToVVE(df), 'Unsupported OS (Windows required)')
-  }
+  expect_error(ExportDataframeToVVE(df, NULL), 'Invalid filename')
+  expect_error(ExportDataframeToVVE(df, ""), 'Invalid filename')
+  expect_error(ExportDataframeToVVE(df, df), 'Invalid filename')
 })
 #> Test passed 😸
 
